e1cfa408d2503abd4535e2ec866ad90ca920643f
20dinara03 <xgarip00@stud.fit.vutbr.cz>
2024-11-28 01:13:14 +0100

    Changes

--- BlazorSignalRApp/BlazorSignalRApp.Client/obj/Debug/net8.0/BlazorSignalRApp.Client.AssemblyInfo.cs
+++ BlazorSignalRApp/BlazorSignalRApp.Client/obj/Debug/net8.0/BlazorSignalRApp.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorSignalRApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daae9e905f483bb2e70dcb4a6abe39b5d9d98ab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorSignalRApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorSignalRApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
